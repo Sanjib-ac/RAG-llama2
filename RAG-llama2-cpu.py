305250e812cdc881b0ceff3f2ccb73e4423c7723
SanjibSarkarU <sanjib.ut@gmail.com>
2024-03-25 09:14:01 -0400

    data folder

--- RAG-llama2-cpu.py
+++ RAG-llama2-cpu.py
@@ -5,14 +5,15 @@ pip install install sentence_transformers  #Embedding
 pip install llama-index-llms-huggingface
 pip install llama_index
 pip install llama-index-embeddings-langchain
+pip install accelerate
+pip install -i https://pypi.org/simple/ bitsandbytes
+pip install transformers==4.30
 """
 
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, PromptTemplate
 from llama_index.llms.huggingface import HuggingFaceLLM
 from llama_index.core.prompts.prompts import SimpleInputPrompt
-
-# from llama_index.llms import HuggingFaceLLM
-# from llama_index.prompts.prompts import SimpleInputPrompt
+import llama_index.core.settings as lis
 
 documents = SimpleDirectoryReader("./data").load_data()
 # print(documents)
@@ -27,22 +28,22 @@ print(query_wrapper_prompt)
 
 from huggingface_hub import login
 
-login(token="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")
+login(token="xxxxxxxxxxxxxxx")
 
 import torch
 
 llm = HuggingFaceLLM(
-    context_window=4096,
-    max_new_tokens=256,
-    generate_kwargs={"temperature": 0.0, "do_sample": False},
+    context_window=2048,
+    max_new_tokens=128,
+    generate_kwargs={"temperature": 0.1},
     system_prompt=system_prompt,
     query_wrapper_prompt=query_wrapper_prompt,
     tokenizer_name="meta-llama/Llama-2-7b-chat-hf",
     model_name="meta-llama/Llama-2-7b-chat-hf",
     device_map="auto",
     # uncomment this if using CUDA to reduce memory usage
-    model_kwargs={"torch_dtype": torch.float16, "load_in_8bit": False}
-)
+    # model_kwargs={"torch_dtype": torch.float16, "load_in_8bit": True}
+)  # , "load_in_8bit_fp32_cpu_offload": True
 
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from llama_index.core import ServiceContext
@@ -52,14 +53,23 @@ from llama_index.core import ServiceContext
 embed_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
 service_context = ServiceContext.from_defaults(
-    chunk_size=1024,
+    chunk_size=512,
     llm=llm,
     embed_model=embed_model
 )
 
-index=VectorStoreIndex.from_documents(documents,service_context=service_context)
+# service_context = lis.Settings(
+#     chunk_size=1024,
+#     llm=llm,
+#     embed_model=embed_model
+# )
 
-query_engine=index.as_query_engine()
+print(f'Service Context: {service_context}')
+index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+
+print(f'Index: {index}')
+query_engine = index.as_query_engine()
+print(f'Query:{query_engine}')
 
 response=query_engine.query("what is attention is all you need?")
 
